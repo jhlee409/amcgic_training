96cc094f87770c18cf7a026f721e102b2f81620b
jhlee409 <jhlee409@gmail.com>
2024-04-09 16:26:03 +0900

    d

---
+++ test.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import time
+from PIL import Image
+import docx
+import io
+from openai import OpenAI
+import firebase_admin
+from firebase_admin import credentials, initialize_app, storage
+
+# Set page to wide mode
+st.set_page_config(page_title="EGD_Dx", layout="wide")
+
+if st.session_state.get('logged_in'):
+
+    # Initialize prompt variable
+    prompt = ""      
+
+    # Check if Firebase app has already been initialized
+    if not firebase_admin._apps:
+        # Streamlit Secrets에서 Firebase 설정 정보 로드
+        cred = credentials.Certificate({
+            "type": "service_account",
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"].replace('\\n', '\n'),
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+            "universe_domain": st.secrets["universe_domain"]
+        })
+        firebase_admin.initialize_app(cred, {'storageBucket': 'amcgi-bulletin.appspot.com'})
+
+    # Firebase Storage에서 MP4 파일의 URL을 검색합니다.
+bucket = storage.bucket()
+blob = bucket.blob("EGD_variation/맨_처음_보세요.mp4")
+video_url = blob.generate_signed_url(expiration=timedelta(seconds=300), method='GET')
+
+# Streamlit에서 동영상을 표시합니다.
+st.video(video_url)
