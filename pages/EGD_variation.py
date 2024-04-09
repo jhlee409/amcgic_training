20efe5595180b77abb4dfd07ee8b8a1eb4f522ac
jhlee409 <jhlee409@gmail.com>
2024-04-09 14:13:28 +0900

    d

--- pages/EGD_variation.py
+++ pages/EGD_variation.py
@@ -66,7 +66,8 @@ if st.session_state.get('logged_in'):
         '<a href="https://youtu.be/iHUXGo1lEcw" target="_blank">Link 1</a>, <a href="https://youtu.be/o9XbvVpv4I4" target="_blank">Link 2</a>', #H
         '<a href="https://youtu.be/3I0zq0FEKLU" target="_blank">Link 1</a>, <a href="https://youtu.be/KFRYOBTgHOE" target="_blank">Link 2</a>', #I
         '<a href="https://youtu.be/5Hxo44wUQkQ" target="_blank">Link 1</a>, <a href="https://youtu.be/iUAbnLxuZcQ" target="_blank">Link 2</a>, <a href="https://youtu.be/YXOoFj5CFjs" target="_blank">Link 3</a>, <a href="https://youtu.be/GGtzY-5vBFM" target="_blank">Link 4</a>, <a href="https://youtu.be/J1jKzg3keHQ" target="_blank">Link 5</a>', #J
-        '<a href="https://youtu.be/3I0zq0FEKLU" target="_blank">Link 1</a>, <a href="https://youtu.be/KFRYOBTgHOE" target="_blank">Link 2</a>', #I
+        '<a href="https://youtu.be/-q9-hOskzmI" target="_blank">Link 1</a>', #K
+        '<a href="https://youtu.be/-q9-hOskzmI" target="_blank">Link 1</a>, <a href="https://youtu.be/KFRYOBTgHOE" target="_blank">Link 2</a>', #I
         
         # ... 나머지 항목에 해당하는 markdown 텍스트 추가
     ]
