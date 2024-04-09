6c76f24ee9f8f0cd5f70bf7919c86b13fa3c0b77
jhlee409 <jhlee409@gmail.com>
2024-04-09 12:24:16 +0900

    ㅇ

--- pages/EGD_variation.py
+++ pages/EGD_variation.py
@@ -44,28 +44,28 @@ if st.session_state.get('logged_in'):
 
     # 23개 항목의 데이터
     data = [
-        '가장 먼저 보세요', 
-        '사진이 흔들려서 찍힘',
-        '과도한 retching',
-        '진정 시 너무 irritable하다',
-        '좌우가 바뀌어 있다(situs inversus)',
-        '위안에 음식물이 남아있다',
-        '정상 위에서 검사 전과정',
+        '가장 먼저 보세요: 전체 과정 해설', 
+        'EGD 사진이 흔들려서 찍히는 경우가 많아요',
+        '환자가 과도한 retching을 해서 검사의 진행이 어려워요',
+        '진정 내시경 시 환자가 너무 irritable해서 검사의 진행이 어려워요',
+        '장기의 좌우가 바뀌어 있다(situs inversus)',
+        '위로 진입해 보니, 위안에 음식물이 남아있다',
+        '정상 위에서 Expert의 검사 전과정',
         'STG with Bilroth II reconstruction state애서 검사 전과정',
         'STG with Bilroth I reconstruction state에서 검사 전과정',
         '후두부 접근 시 구역이 심해 후두를 관찰할 수 없다',
         'epiglotis가 닫혀서 후부두 전체가 보이는 사진을 찍을 수가 없다',
         '식도가 너무 tortuous 해서 화면 중앙에 놓고 전진하기 힘든다',
-        'z line이 stomach 쪽으로 내려가 있어 보이지 않는다',
+        'z line이 stomach 쪽으로 내려가 있어 z line이 보이지 않는다',
         'fundus, HB 경계부위가 심하게 꺽어져 있어, antrum 쪽으로 진입이 안된다',
-        'pyloric ring이 계속 닫혀있고 움직여서 내시경 통과가 어렵다',
+        'pyloric ring이 계속 닫혀있고 움직여서 scope의 통과가 어렵다',
         '십이지장 벽에 닿기만 하고, SDA의 위치를 찾지 못하겠다',
-        'SDA가 사진 상 우측이 아니라 좌측에 있다.',
-        '제2부에서 scope를 당기면 전진하지 않고 그냥 빠진다',
+        'bulb에 들어가 보니, SDA가 사진 상 우측이 아니라 좌측에 있다.',
+        '제2부에서 scope를 당기면 전진해야 하는데, 전진하지 않고 그냥 빠진다',
         '십이지장 2nd portion인데, ampulla가 안보이는데 prox 쪽에 있는 것 같다',
         'minor papilla를 AOP로 착각하지 않으려면',
         'antrum GC에 transverse fold가 있어 그 distal part 부분이 가려져 있다',
-        '전정부에서 노브를 up을 했는데도, angle을 관찰할 수 없다',
+        '전정부에서 노브를 up을 했는데도, antrum에 붙어서, angle을 관찰할 수 없다',
         '환자의 belcing이 너무 심해 공기가 빠져 fold가 펴지지 않는다'
     ]
 
@@ -87,7 +87,7 @@ if st.session_state.get('logged_in'):
     # 제목과 23개 항목 출력
     st.header('제목')
     for item in data:
-        cols = st.columns([1, 1])  # 2개의 컬럼을 1:1 비율로 생성
+        cols = st.columns([1, 2])  # 2개의 컬럼을 1:1 비율로 생성
         cols[0].write(item)
         cols[1].write("Link 1, Link 2, Link 3, Link 4, Link 5")
 
