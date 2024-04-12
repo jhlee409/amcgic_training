def4ba58cf2b5286c6de9894947993c82da7825f
jhlee409 <jhlee409@gmail.com>
2024-04-12 09:51:04 +0900

    d

--- pages/URL_allocation.py
+++ pages/URL_allocation.py
@@ -7,24 +7,15 @@ import streamlit as st
 # Set page to wide mode
 st.set_page_config(page_title="URL_allocation", layout="wide")
 
-if st.session_state.get('logged_in'):
+# 로그인 상태 확인
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
 
+if st.session_state.logged_in:
     # Check if Firebase app has already been initialized
     if not firebase_admin._apps:
-        # Streamlit Secrets에서 Firebase 설정 정보 로드
-        cred = credentials.Certificate({
-            "type": "service_account",
-            "project_id": st.secrets["project_id"],
-            "private_key_id": st.secrets["private_key_id"],
-            "private_key": st.secrets["private_key"].replace('\\n', '\n'),
-            "client_email": st.secrets["client_email"],
-            "client_id": st.secrets["client_id"],
-            "auth_uri": st.secrets["auth_uri"],
-            "token_uri": st.secrets["token_uri"],
-            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-            "universe_domain": st.secrets["universe_domain"]
-        })
+        # Firebase 설정 정보 로드
+        cred = credentials.Certificate('"C:\Users\admin\OneDrive - UOU\LLM\project_bulletin\code_bulletin_all_user_erase_registration\amcgi-bulletin-4f317f4638ed.json"')
         firebase_admin.initialize_app(cred)
 
     # Firebase Storage에서 MP4 파일의 URL을 검색합니다.
