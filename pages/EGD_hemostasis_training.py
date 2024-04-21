4aa332c4e7d25229898230e119e53b0798eacf67
jhlee409 <jhlee409@gmail.com>
2024-04-22 05:49:10 +0900

    d

--- pages/EGD_hemostasis_training.py
+++ pages/EGD_hemostasis_training.py
@@ -93,10 +93,10 @@ if st.session_state.get('logged_in'):
         expiration_time = datetime.utcnow() + timedelta(seconds=600)
         video_url = blob_a1.generate_signed_url(expiration=expiration_time, method='GET')
         st.write(video_url)
-        st.video(video_url)
+        #st.video(video_url)
         # 동영상 플레이어의 크기를 조정하기 위해 HTML 코드 사용
-        # video_html = f'<video width="600" controls><source src="{video_url}" type="video/mp4"></video>'
-        # st.markdown(video_html, unsafe_allow_html=True)
+        video_html = f'<video width="600" controls><source src="{video_url}" type="video/mp4"></video>'
+        st.markdown(video_html, unsafe_allow_html=True)
 
     # Function to list files in a specific directory in Firebase Storage
     def list_files(bucket_name, directory):
