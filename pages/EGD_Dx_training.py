3984a48e677d20a3ba32bfb5037e105cacdd7033
jhlee409 <jhlee409@gmail.com>
2024-04-24 12:13:26 +0900

    d

--- pages/EGD_Dx_training.py
+++ pages/EGD_Dx_training.py
@@ -95,7 +95,7 @@ if selected_image_file:
     st.image(image, width=display_width)
 
     # Find the corresponding docx file
-    docx_file_name = os.path.splitext(selected_image_file)[0] + '.docx'
+    docx_file_name = os.path.splitext(selected_image_file)[0] + '_1.docx'
     docx_file_path = directory_instructions + docx_file_name
     
     # Download and read the contents of the docx file
@@ -108,8 +108,7 @@ if selected_image_file:
     doc = docx.Document(docx_stream)
     docx_content = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
     
-    st.write("Instructions:")
-    st.write(docx_content)
+    st.markdown(docx_content)
 
 st.sidebar.divider()
 
