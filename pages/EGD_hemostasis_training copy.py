693bf7dec8f5ebe4dadcfa45620de44e31a0ee5d
jhlee409 <jhlee409@gmail.com>
2024-04-26 20:39:09 +0900

    d

--- pages/EGD_hemostasis_training copy.py
+++ pages/EGD_hemostasis_training copy.py
@@ -129,34 +129,26 @@ if st.session_state.get('logged_in'):
             if selected_instruction_file1:
                 full_path1 = selected_instruction_file1
                 prompt1 = read_docx_file('amcgi-bulletin.appspot.com', full_path1)
-                prompt1_lines = prompt1.split('\n')  # 내용을 줄 바꿈 문자로 분리
-                prompt1_markdown = '\n'.join(prompt1_lines)  # 분리된 내용을 다시 합치면서 줄 바꿈 적용
-                st.markdown(prompt1_markdown)
+                st.text(prompt1)  # Display the content of the docx file as text
             
             # 이전 동영상 플레이어 지우기
             pre_video_container.empty()
             
-            # 새로운 동영상 플레이어 렌더링
-            with pre_video_container:
-                video_html = f'<video width="500" controls><source src="{st.session_state.pre_video_url}" type="video/mp4"></video>'
-                st.markdown(video_html, unsafe_allow_html=True)
-
-                instruction_file_name2 = os.path.splitext(selected_pre_videos_file)[0] + '_2.docx'
-                selected_instruction_file2 = directory_instructions + instruction_file_name2
-
-                # '진행' 버튼 추가
-                if st.sidebar.button('진행'):
-                    if selected_instruction_file2:
-                        full_path2 = selected_instruction_file2
-                        prompt2 = read_docx_file('amcgi-bulletin.appspot.com', full_path2)
-                        prompt2_lines = prompt2.split('\n')  # 내용을 줄 바꿈 문자로 분리
-                        prompt2_markdown = '\n'.join(prompt2_lines)  # 분리된 내용을 다시 합치면서 줄 바꿈 적용
-                        
-                        # 새로운 컨테이너에 file2의 내용 출력
-                        with st.container():
-                            st.markdown(prompt2_markdown)  # Display the content of the docx file with line breaks
-                    
-                    st.session_state['show_expander'] = True
+        # 새로운 동영상 플레이어 렌더링
+        with pre_video_container:
+            video_html = f'<video width="500" controls><source src="{st.session_state.pre_video_url}" type="video/mp4"></video>'
+            st.markdown(video_html, unsafe_allow_html=True)
+        
+            instruction_file_name2 = os.path.splitext(selected_pre_videos_file)[0] + '_2.docx'
+            selected_instruction_file2 = directory_instructions + instruction_file_name2
+                
+            # '진행' 버튼 추가
+            if st.sidebar.button('진행'):
+                if selected_instruction_file2:
+                    full_path2 = selected_instruction_file2
+                    prompt2 = read_docx_file('amcgi-bulletin.appspot.com', full_path2)
+                    st.text(prompt2)  # Display the content of the docx file as text
+                st.session_state['show_expander'] = True
 
     st.sidebar.divider()
 
