31bb6c637416b3322758c8dd9bac10f0bc8472ec
jhlee409 <jhlee409@gmail.com>
2024-05-02 14:42:25 +0900

    e

---
+++ Login copy.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import requests
+import json
+
+st.set_page_config(page_title="GI_training", layout="wide")
+
+# 제목 및 서브헤딩 설정
+st.title("GI training programs")
+
+# 설명 텍스트
+with st.expander("**이 프로그램 사용 방법**"):
+    st.write("* 로그인이 제대로 안되면 왼쪽 증례 페이지에 접근할 수 없습니다.")
+    st.write("* 알려드린 id와 pw로 로그인 하신 후 왼쪽 sidebar에서 원하는 프로그램을 선택하면 그 페이지로 이동합니다.")
+    st.write("* 이 프로그램은 울산의대 서울아산병원 이진혁과 의대 관계자 및 다른 서울아산병원 소화기 선생님들의 참여에 의해 제작되었습니다.")
+st.divider()
+
+# 사용자 인풋
+email = st.text_input("Email")
+password = st.text_input("Password", type="password")
+
+# 로그인 버튼
+if st.button("Login"):
+    try:
+        # Streamlit secret에서 Firebase API 키 가져오기
+        api_key = st.secrets["FIREBASE_API_KEY"]
+        request_ref = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
+        headers = {"Content-Type": "application/json"}
+        data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
+
+        response = requests.post(request_ref, headers=headers, data=data)
+        response_data = response.json()
+
+        if response.status_code == 200:
+            st.success(f"{email}님, 로그인에 성공하셨습니다. 이제 왼쪽의 메뉴를 이용하실 수 있습니다.")
+            st.session_state['logged_in'] = True 
+        else:
+            st.error(response_data["error"]["message"])
+    except Exception as e:
+        st.error("An error occurred: " + str(e))
+
+# 로그 아웃 버튼
+if "logged_in" in st.session_state and st.session_state['logged_in']:
+    
+    if st.sidebar.button("Logout"):
+        st.session_state['logged_in'] = False
+        st.success("로그아웃 되었습니다.")
+        # 필요시 추가적인 세션 상태 초기화 코드
+        # 예: del st.session_state['logged_in']
+
+    st.write("마지막 수정 날짜 및 수정사항: 2024년 2월 15일; EGD 사진 진단 훈련 프로그램 내용 삭제")
