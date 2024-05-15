6a73bbe02229bab57f21424d212b467092ce0d60
jhlee409 <jhlee409@gmail.com>
2024-05-15 09:48:47 +0900

    d

--- pages/EGD_hemostasis_training.py
+++ pages/EGD_hemostasis_training.py
@@ -160,7 +160,8 @@ if st.session_state.get('logged_in'):
 
                 # 비디오 플레이어 삽입
                 with video_player_container:
-                    st.video(video_url, format='video/mp4', height=1000)
+                    video_html = f'<video width="1000" height="1000" controls><source src="{video_url}" type="video/mp4"></video>'
+                    st.markdown(video_html, unsafe_allow_html=True)
 
             if folder_selection == "초기화":
                 st.empty()  # 동영상 플레이어 제거
