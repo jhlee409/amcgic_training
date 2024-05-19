cb944e6ee08d43ed95b0fe6ba83ad50d19c81dce
jhlee409 <jhlee409@gmail.com>
2024-05-19 09:52:05 +0900

    d

--- pages/EGD_hemostasis_training.py
+++ pages/EGD_hemostasis_training.py
@@ -154,15 +154,9 @@ if st.session_state.get('logged_in'):
             with pre_video_container:
                 video_html = f'<video width="500" height="500" controls><source src="{st.session_state.pre_video_url}" type="video/mp4"></video>'
                 st.markdown(video_html, unsafe_allow_html=True)
-
-                if st.session_state.get('previous_folder_selection', None) != folder_selection:
-                    st.session_state.previous_folder_selection = folder_selection
-                    pre_video_container.empty()
-                    video_player_container.empty()
-                    st.empty()
                     
-        instruction_file_name = os.path.splitext(selected_pre_videos_file)[0] + '.docx'
-        selected_instruction_file = directory_instructions + instruction_file_name
+            instruction_file_name = os.path.splitext(selected_pre_videos_file)[0] + '.docx'
+            selected_instruction_file = directory_instructions + instruction_file_name
 
         # '진행' 버튼 추가
         if st.sidebar.button('진행'):
