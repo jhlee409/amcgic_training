d9500b229c43ebb58c7f00eac3e8650d7a9efee7
jhlee409 <jhlee409@gmail.com>
2024-05-19 09:05:35 +0900

    d

--- pages/EGD_hemostasis_training.py
+++ pages/EGD_hemostasis_training.py
@@ -140,10 +140,10 @@ if st.session_state.get('logged_in'):
             pre_video_container.empty()
             video_player_container.empty()
             
-        # 새로운 동영상 플레이어 렌더링        
-        with pre_video_container:
-            video_html = f'<video width="500" height="500" controls><source src="{st.session_state.pre_video_url}" type="video/mp4"></video>'
-            st.markdown(video_html, unsafe_allow_html=True)
+            # 새로운 동영상 플레이어 렌더링        
+            with pre_video_container:
+                video_html = f'<video width="500" height="500" controls><source src="{st.session_state.pre_video_url}" type="video/mp4"></video>'
+                st.markdown(video_html, unsafe_allow_html=True)
                     
         instruction_file_name = os.path.splitext(selected_pre_videos_file)[0] + '.docx'
         selected_instruction_file = directory_instructions + instruction_file_name
