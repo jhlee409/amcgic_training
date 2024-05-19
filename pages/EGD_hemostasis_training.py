412abec8923b7d0f27601f3cf8aa5402371f26c0
jhlee409 <jhlee409@gmail.com>
2024-05-19 10:47:39 +0900

    d

--- pages/EGD_hemostasis_training.py
+++ pages/EGD_hemostasis_training.py
@@ -157,6 +157,9 @@ if st.session_state.get('logged_in'):
         if folder_selection == "초기화":
             st.empty()  # 동영상 플레이어 제거
 
+    if folder_selection == "초기화":
+        st.empty()  # 동영상 플레이어 제거
+
     instruction_file_name = os.path.splitext(selected_pre_videos_file)[0] + '.docx'
     selected_instruction_file = directory_instructions + instruction_file_name
 
