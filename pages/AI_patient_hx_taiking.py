314e093aa183994e03274ab063d372fb107d2eab
jhlee409 <jhlee409@gmail.com>
2024-06-11 14:39:10 +0900

    d

--- pages/AI_patient_hx_taiking.py
+++ pages/AI_patient_hx_taiking.py
@@ -162,58 +162,58 @@ if st.session_state.get('logged_in'):
             st.write("- 증례 해설 자료가 필요하시면 다운로드 하실 수 있는데, 전체가 refresh 되므로 도중에 다울로드 하지 마시고, 마지막에 다운로드 받아주세요.")
         main_container.divider()
 
-        # Get user input from chat input
-        user_input = main_container.chat_input("입력창입니다. 선생님의 message를 여기에 입력하고 엔터를 치세요")
+    # Get user input from chat input
+    user_input = main_container.chat_input("입력창입니다. 선생님의 message를 여기에 입력하고 엔터를 치세요")
 
-        # 사용자 입력이 있을 경우, prompt를 user_input으로 설정
-        if user_input:
-            prompt = user_input
+    # 사용자 입력이 있을 경우, prompt를 user_input으로 설정
+    if user_input:
+        prompt = user_input
 
-        message = client.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=prompt
-        )
-        #RUN을 돌리는 과정
-        run = client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=assistant_id,
-        )
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=prompt
+    )
+    #RUN을 돌리는 과정
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+    )
 
-        with st.spinner('열일 중...'):
-            #RUN이 completed 되었나 1초마다 체크
-            while run.status != "completed":
-                time.sleep(1)
-                run = client.beta.threads.runs.retrieve(
-                    thread_id=thread_id,
-                    run_id=run.id
-                )
+    with st.spinner('열일 중...'):
+        #RUN이 completed 되었나 1초마다 체크
+        while run.status != "completed":
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run.id
+            )
 
-        #while문을 빠져나왔다는 것은 완료됐다는 것이니 메세지 불러오기
-        messages = client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
+    #while문을 빠져나왔다는 것은 완료됐다는 것이니 메세지 불러오기
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id
+    )
 
-        #메세지 모두 불러오기
-        thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
+    #메세지 모두 불러오기
+    thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
 
-        st.sidebar.divider()
+    st.sidebar.divider()
 
-        # Clear button in the sidebar
-        if st.sidebar.button('이전 대화기록 삭제 버튼'):
-            # Reset the prompt, create a new thread, and clear the docx_file and messages
-            prompt = []
-            thread = client.beta.threads.create()
-            st.session_state.thread_id = thread.id
-            st.session_state['messages'] = []
-            for msg in thread_messages.data:
-                msg.content[0].text.value=""
+    # Clear button in the sidebar
+    if st.sidebar.button('이전 대화기록 삭제 버튼'):
+        # Reset the prompt, create a new thread, and clear the docx_file and messages
+        prompt = []
+        thread = client.beta.threads.create()
+        st.session_state.thread_id = thread.id
+        st.session_state['messages'] = []
+        for msg in thread_messages.data:
+            msg.content[0].text.value=""
 
-        st.sidebar.divider()
-        # 로그아웃 버튼 생성
-        if st.sidebar.button('로그아웃'):
-            st.session_state.logged_in = False
-            st.experimental_rerun()  # 페이지를 새로고침하여 로그인 화면으로 돌아감
+    st.sidebar.divider()
+    # 로그아웃 버튼 생성
+    if st.sidebar.button('로그아웃'):
+        st.session_state.logged_in = False
+        st.experimental_rerun()  # 페이지를 새로고침하여 로그인 화면으로 돌아감
 
     # assistant 메시지를 메시지 창에 추가
     if message.content and message.content[0].text.value and '전체 지시 사항' not in message.content[0].text.value:
