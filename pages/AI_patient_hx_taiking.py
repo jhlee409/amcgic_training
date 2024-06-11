2464c72e7f154dbc3dd317a93eef35be9ae760ea
jhlee409 <jhlee409@gmail.com>
2024-06-11 14:10:20 +0900

    d

--- pages/AI_patient_hx_taiking.py
+++ pages/AI_patient_hx_taiking.py
@@ -210,7 +210,7 @@ if st.session_state.get('logged_in'):
 
     # assistant 메시지를 메시지 창에 추가
     if message.content and message.content[0].text.value and '전체 지시 사항' not in message.content[0].text.value:
-        message_box.text(f"**{messages.data[0].role}:** {messages.data[0].content[0].text.value}")
+        message_box.markdown(f"**{messages.data[0].role}:** {messages.data[0].content[0].text.value}")
 
 else:
     # 로그인이 되지 않은 경우, 로그인 페이지로 리디렉션 또는 메시지 표시
