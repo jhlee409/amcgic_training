1d603eb6d94ebfd553f71957898c9ba0d52f80f1
jhlee409 <jhlee409@gmail.com>
2024-06-11 12:36:42 +0900

    d

--- pages/AI_patient_hx_taiking.py
+++ pages/AI_patient_hx_taiking.py
@@ -165,6 +165,15 @@ if st.session_state.get('logged_in'):
 
     st.sidebar.divider()
     
+    for msg in thread_messages.data:
+        # 메시지 내용 확인 및 필터링 조건 추가
+        if msg.content and msg.content[0].text.value:
+            content = msg.content[0].text.value
+            # 필터링 조건: 내용이 비어있지 않고, '..', '...', '전체 지시 사항'을 포함하지 않는 경우에만 UI에 표시
+            if content.strip() not in ['', '..', '...'] and '전체 지시 사항' not in content:
+                with st.chat_message(msg.role):
+                    st.write(content)
+    
     # 로그아웃 버튼 생성
     if st.sidebar.button('로그아웃'):
         st.session_state.logged_in = False
@@ -175,16 +184,6 @@ if st.session_state.get('logged_in'):
     #     with st.chat_message(messages.data[0].role):
     #         st.write(messages.data[0].content[0].text.value)
 
-    for msg in thread_messages.data:
-        # 메시지 내용 확인 및 필터링 조건 추가
-        if msg.content and msg.content[0].text.value:
-            content = msg.content[0].text.value
-            # 필터링 조건: 내용이 비어있지 않고, '..', '...', '전체 지시 사항'을 포함하지 않는 경우에만 UI에 표시
-            if content.strip() not in ['', '..', '...'] and '전체 지시 사항' not in content:
-                with st.chat_message(msg.role):
-                    st.write(content)
-
-            
 else:
     # 로그인이 되지 않은 경우, 로그인 페이지로 리디렉션 또는 메시지 표시
     st.error("로그인이 필요합니다.")
