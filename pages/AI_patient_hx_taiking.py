2acd3b65a0b4dcee444635d10166054777eb28de
jhlee409 <jhlee409@gmail.com>
2024-10-18 08:47:29 +0900

    d

--- pages/AI_patient_hx_taiking.py
+++ pages/AI_patient_hx_taiking.py
@@ -225,7 +225,7 @@ if st.session_state.get('logged_in'):
     # 로그아웃 버튼 생성
     if st.sidebar.button('로그아웃'):
         st.session_state.logged_in = False
-        st.experimental_rerun()  # 페이지를 새로고침하여 로그인 화면으로 돌아감
+        st.rerun()  # 페이지를 새로고침하여 로그인 화면으로 돌아감
 
 else:
     # 로그인이 되지 않은 경우, 로그인 페이지로 리디렉션 또는 메시지 표시
