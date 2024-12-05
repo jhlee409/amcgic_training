95b78e4095156473ca3fbf1249ffcb1dc854ce9e
jhlee409 <jhlee409@gmail.com>
2024-12-05 20:05:22 +0900

    9

--- pages/PBL.py
+++ pages/PBL.py
@@ -158,19 +158,19 @@ if st.session_state.get('logged_in'):
 
     st.write(assistant_id)
     
-    # 사용자 입력이 있을 경우, prompt를 user_input으로 설정
-    if user_input:
-        prompt = user_input
+    # # 사용자 입력이 있을 경우, prompt를 user_input으로 설정
+    # if user_input:
+    #     prompt = user_input
 
-    message = client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=prompt
-    )
-    #RUN을 돌리는 과정
-    run = client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant_id,
+    # message = client.beta.threads.messages.create(
+    #     thread_id=thread_id,
+    #     role="user",
+    #     content=prompt
+    # )
+    # #RUN을 돌리는 과정
+    # run = client.beta.threads.runs.create(
+    #     thread_id=thread_id,
+    #     assistant_id=assistant_id,
     )
 
     # with st.spinner('열일 중...'):
