9829ec41e17b58aa58099598a9f8b8b6d1461241
jhlee409 <jhlee409@gmail.com>
2024-12-05 21:49:01 +0900

    7

--- pages/PBL.py
+++ pages/PBL.py
@@ -164,41 +164,41 @@ if st.session_state.get('logged_in'):
         if user_input:
             prompt = user_input
 
-        message = client.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=prompt
-        )
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=prompt
+    )
 
-        #RUN을 돌리는 과정
-        run = client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=assistant_id,
-        )
+    #RUN을 돌리는 과정
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+    )
 
-        with st.spinner('열일 중...'):
-            #RUN이 completed 되었나 1초마다 체크
-            while run.status != "completed":
-                time.sleep(1)
-                run = client.beta.threads.runs.retrieve(
-                    thread_id=thread_id,
-                    run_id=run.id
-                )
+    with st.spinner('열일 중...'):
+        #RUN이 completed 되었나 1초마다 체크
+        while run.status != "completed":
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run.id
+            )
 
-        #while문을 빠져나왔다는 것은 완료됐다는 것이니 메세지 불러오기
-        messages = client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
-        
-        # message 변수가 정의된 후에만 사용
-        if message.content and message.content[0].text.value and '전체 지시 사항' not in message.content[0].text.value:
-            if messages.data[0].role == "assistant":
-                st.session_state.message_box += f"🤖: {messages.data[0].content[0].text.value}\n\n"
-        
-        st.write(assistant_id)
+    #while문을 빠져나왔다는 것은 완료됐다는 것이니 메세지 불러오기
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id
+    )
+    
+    # message 변수가 정의된 후에만 사용
+    if message.content and message.content[0].text.value and '전체 지시 사항' not in message.content[0].text.value:
+        if messages.data[0].role == "assistant":
+            st.session_state.message_box += f"🤖: {messages.data[0].content[0].text.value}\n\n"
+    
+    st.write(assistant_id)
 
-        #메세지 모두 불러오기
-        thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
+    #메세지 모두 불러오기
+    thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
 
     st.sidebar.divider()
 
@@ -213,18 +213,18 @@ if st.session_state.get('logged_in'):
 #         message_container.markdown(st.session_state.message_box, unsafe_allow_html=True)
 
 
-    #메세지 모두 불러오기
-    thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
+    # #메세지 모두 불러오기
+    # thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
 
-    for msg in thread_messages.data:
-        # 메시지 내용 확인 및 필터링 조건 추가
-        if msg.content and msg.content[0].text.value:
-            content = msg.content[0].text.value
-            # 필터링 조건: 내용이 비어있지 않고, '..', '...', '전체 지시 사항'을 포함하지 않는 경우에만 UI에 표시
-            if content.strip() not in ['', '..', '...'] and '전체 지시 사항' not in content:
-                if msg.role != 'user':
-                    with st.chat_message(msg.role):
-                        st.write(content)
+    # for msg in thread_messages.data:
+    #     # 메시지 내용 확인 및 필터링 조건 추가
+    #     if msg.content and msg.content[0].text.value:
+    #         content = msg.content[0].text.value
+    #         # 필터링 조건: 내용이 비어있지 않고, '..', '...', '전체 지시 사항'을 포함하지 않는 경우에만 UI에 표시
+    #         if content.strip() not in ['', '..', '...'] and '전체 지시 사항' not in content:
+    #             if msg.role != 'user':
+    #                 with st.chat_message(msg.role):
+    #                     st.write(content)
                         
         # Clear button in the sidebar
     if st.sidebar.button('이전 대화기록 삭제 버튼'):
