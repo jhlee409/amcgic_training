80ddf90c0909a8a161a525e8943cd5cadfa9044f
jhlee409 <jhlee409@gmail.com>
2024-12-05 20:16:14 +0900

    6

--- pages/PBL.py
+++ pages/PBL.py
@@ -162,14 +162,11 @@ if st.session_state.get('logged_in'):
     if user_input:
         prompt = user_input
 
-    try:
-        message = client.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=prompt
-        )
-    except Exception as e:
-        print(f"에러 발생: {str(e)}")
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=prompt
+    )
     
     #RUN을 돌리는 과정
     run = client.beta.threads.runs.create(
