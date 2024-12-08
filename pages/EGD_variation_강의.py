47bba5d2ebd098d3607125a1f9d99c177912a57a
jhlee409 <jhlee409@gmail.com>
2024-12-08 15:24:20 +0900

    4

--- pages/EGD_variation_강의.py
+++ pages/EGD_variation_강의.py
@@ -9,7 +9,7 @@ import firebase_admin
 from firebase_admin import credentials, initialize_app, storage
 
 # Set page to wide mode
-st.set_page_config(page_title="EGD_Varation 강의", layout="wide")
+st.set_page_config(page_title="EGD_Varation", layout="wide")
 
 
 if st.session_state.get('logged_in'):
@@ -60,8 +60,11 @@ if st.session_state.get('logged_in'):
             grouped_videos[first_letter] = []
         grouped_videos[first_letter].append(video)
 
-    st.header("EGD Variation Video Player")
+    st.header("EGD variation 강의")
     st.write("아래 버튼을 눌러 동영상을 시청하세요:")
+    
+    with st.expander(" 필독!!! 먼저 여기를 눌러 사용방법을 확인하세요."):
+        st.write("- 해당 주제에 대해 다른 증례를 해설하는 동양상의 버튼이 오른쪽이 있습니다. 버튼을 눌러 동영상을 시청하세요.")
 
     # 각 그룹을 8개의 열에 배치
     for letter, videos in grouped_videos.items():
