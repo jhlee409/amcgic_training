54a9db5d903d64c87787186378190172852d85bb
jhlee409 <jhlee409@gmail.com>
2024-12-08 18:40:00 +0900

    0

--- pages/PEG_Dx_EUS_강의.py
+++ pages/PEG_Dx_EUS_강의.py
@@ -70,7 +70,7 @@ if st.session_state.get('logged_in'):
 
             # Firebase Storage에 로그 파일 업로드
             bucket = storage.bucket('amcgi-bulletin.appspot.com')  # Firebase Storage 버킷 참조
-            log_blob = bucket.blob(f'log_PEG_EUS/{user_email}_lecture_{selected_lecture}.txt')  # 로그 파일 경로 설정
+            log_blob = bucket.blob(f'log_PEG_EUS/{user_email}_{selected_lecture}.txt')  # 로그 파일 경로 설정
             log_blob.upload_from_string(log_entry, content_type='text/plain')  # 문자열로 업로드
 
 
