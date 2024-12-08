732fd5bdb6e5bd078c814f90de2a71981f83c9dd
jhlee409 <jhlee409@gmail.com>
2024-12-08 20:31:47 +0900

    1

--- pages/AI_patient_Hx_taking_훈련.py
+++ pages/AI_patient_Hx_taking_훈련.py
@@ -137,7 +137,7 @@ if st.session_state.get('logged_in'):
 
                 # Firebase Storage에 로그 파일 업로드
                 bucket = storage.bucket('amcgi-bulletin.appspot.com')  # Firebase Storage 버킷 참조
-                log_blob = bucket.blob(f'log_AI_patient_Hx_taking/{user_email}_Hx_{file_name_without_extension}.txt')  # 로그 파일 경로 설정
+                log_blob = bucket.blob(f'log_AI_patient_Hx_taking/{user_email}_{file_name_without_extension}.txt')  # 로그 파일 경로 설정
                 log_blob.upload_from_string(log_entry, content_type='text/plain')  # 문자열로 업로드
 
             # Include the directory in the path when reading the file
