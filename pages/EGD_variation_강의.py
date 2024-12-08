90d9017379cd04eca558072553c3922b18a88317
jhlee409 <jhlee409@gmail.com>
2024-12-08 13:45:58 +0900

    0

--- pages/EGD_variation_강의.py
+++ pages/EGD_variation_강의.py
@@ -47,7 +47,7 @@ if st.session_state.get('logged_in'):
         """
         <style>
         .video-player iframe {
-            width: 66vw; /* 화면 가로 길이의 2/3 */
+            width: 180vw;
             height: auto;
         }
         </style>
