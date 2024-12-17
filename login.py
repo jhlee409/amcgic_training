fefcad6e05a03fe6ed3f2a5c19a95a9f4ea1544c
jhlee409 <jhlee409@gmail.com>
2024-12-18 08:04:10 +0900

    7

--- login.py
+++ login.py
@@ -7,7 +7,21 @@ import os
 
 # Firebase 초기화 (아직 초기화되지 않은 경우에만)
 if not firebase_admin._apps:
-    cred = credentials.Certificate(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
+
+    # Streamlit Secrets에서 Firebase 설정 정보 로드
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace('\\n', '\n'),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"]
+    })
     firebase_admin.initialize_app(cred, {
         'databaseURL': st.secrets["FIREBASE_DATABASE_URL"]
     })
