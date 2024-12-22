b6cefb17ac54c4643480e2cc71126c89b88dafb9
jhlee409 <jhlee409@gmail.com>
2024-12-22 10:06:10 +0900

    21

--- pages/Dx_EGD_실전_강의.py
+++ pages/Dx_EGD_실전_강의.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import os
 from PIL import Image
+import docx
+import io
 import firebase_admin
 from firebase_admin import credentials, storage
-from datetime import datetime, timedelta, timezone
-import threading
-import time
-from supabase import create_client, Client
+from datetime import datetime, timedelta
+from pytz import timezone
 
 # Set page to wide mode
 st.set_page_config(page_title="EGD 강의", layout="wide")
 
-if st.session_state.get('logged_in'):
+if st.session_state.get('logged_in'):     
 
     # Check if Firebase app has already been initialized
     if not firebase_admin._apps:
@@ -31,44 +31,13 @@ if st.session_state.get('logged_in'):
         })
         firebase_admin.initialize_app(cred)
 
-    # Supabase 클라이언트 초기화
-    supabase: Client = create_client(
-        st.secrets["supabase_url"],
-        st.secrets["supabase_key"]
-    )
-
-     # UTC 시간 반환
-    def get_utc_time():
-        return datetime.now(timezone.utc)
-
-    # 한국 시간 반환
-    def get_korea_time():
-        korea_tz = timezone(timedelta(hours=9))
-        return datetime.now(korea_tz)
-
-    # Supabase 데이터 업데이트
-    def update_login_data():
-        while st.session_state.get('logged_in'):
-            try:
-                utc_time = get_utc_time()
-                supabase.table('login').insert({
-                    'user_position': st.session_state.get('user_position', 'unknown'),
-                    'user_name': st.session_state.get('user_name', 'unknown'),
-                    'selected_lecture': st.session_state.get('selected_lecture', 'unknown'),
-                    'access_date': utc_time.isoformat()
-                }).execute()
-                time.sleep(60)
-            except Exception as e:
-                print(f"Error updating login data: {e}")
-                time.sleep(60)
-
     # Display Form Title
     st.subheader("EGD 실전 강의 모음")
     with st.expander(" 필독!!! 먼저 여기를 눌러 사용방법을 확인하세요."):
         st.write("- 이 강의 모음은 진단 EGD 실전 강의 동영상 모음입니다.")
         st.write("- 왼쪽에서 시청하고자 하는 강의를 선택한 후 오른쪽 화면에서 강의 첫 화면이 나타나면 화면을 클릭해서 시청하세요.")
         st.write("- 전체 화면을 보실 수 있습니다. 화면 왼쪽 아래 전체 화면 버튼 누르세요.")
-
+          
     # Lectures 폴더 내 mp4 파일 리스트 가져오기  
     def list_mp4_files(bucket_name, directory):
         bucket = storage.bucket(bucket_name)
@@ -79,19 +48,16 @@ if st.session_state.get('logged_in'):
                 file_name = os.path.basename(blob.name)
                 file_names.append(file_name)
         return file_names
-
+    
     # 동영상 플레이어를 렌더링할 컨테이너 생성
     video_player_container = st.container()
-
+    
     # 동영상 플레이어를 렌더링할 플레이스홀더 생성
     video_player_placeholder = st.empty()
 
     # 왼쪽 사이드바에서 강의 선택
     lectures = ["Default", "Description_Impression", "Photo_Report", "Complication_Sedation", "Biopsy_NBI", "Stomach_benign", "Stomach_malignant", "Duodenum", "Lx_Phx_Esophagus", "SET"]
     selected_lecture = st.sidebar.radio("강의를 선택하세요", lectures, index=0)
-
-    # 선택한 강의를 상태로 저장
-    st.session_state['selected_lecture'] = selected_lecture
 
     # 로그 파일 생성
     if selected_lecture:
@@ -100,7 +66,7 @@ if st.session_state.get('logged_in'):
             user_name = st.session_state.get('user_name', 'unknown')
             user_position = st.session_state.get('user_position', 'unknown')
             position_name = f"{user_position}*{user_name}"  # 직책*이름 형식으로 저장
-            access_date = get_utc_time().strftime("%Y-%m-%d")  # 현재 UTC 날짜 가져오기
+            access_date = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d")  # 현재 날짜 가져오기 (시간 제외)
 
             # 로그 내용을 문자열로 생성
             log_entry = f"User: {position_name}, Access Date: {access_date}, 실전강의: {selected_lecture}\n"
@@ -109,6 +75,7 @@ if st.session_state.get('logged_in'):
             bucket = storage.bucket('amcgi-bulletin.appspot.com')  # Firebase Storage 버킷 참조
             log_blob = bucket.blob(f'log_Dx_EGD_실전_강의/{position_name}*{selected_lecture}')  # 로그 파일 경로 설정
             log_blob.upload_from_string(log_entry, content_type='text/plain')  # 문자열로 업로드
+
 
     # 선택된 강의와 같은 이름의 mp4 파일 찾기
     directory_lectures = "Lectures/"
@@ -126,9 +93,9 @@ if st.session_state.get('logged_in'):
         selected_mp4_path = directory_lectures + selected_mp4
         bucket = storage.bucket('amcgi-bulletin.appspot.com')
         blob = bucket.blob(selected_mp4_path)
-        expiration_time = datetime.now(timezone.utc) + timedelta(seconds=1600)  # URL 만료 시간 설정
+        expiration_time = datetime.utcnow() + timedelta(seconds=1600)
         mp4_url = blob.generate_signed_url(expiration=expiration_time, method='GET')
-
+        
         # 동영상 플레이어 렌더링
         with video_player_placeholder.container():
             video_html = f'''
@@ -148,19 +115,13 @@ if st.session_state.get('logged_in'):
     else:
         st.sidebar.warning(f"{selected_lecture}에 해당하는 강의 파일을 찾을 수 없습니다.")
 
-    # 로그인 상태일 때 데이터 업데이트 스레드 시작
-    if 'update_thread' not in st.session_state:
-        update_thread = threading.Thread(target=update_login_data, daemon=True)
-        update_thread.start()
-        st.session_state['update_thread'] = update_thread
-
     st.sidebar.divider()
-
+    
     # 로그아웃 버튼 생성
     if st.sidebar.button('로그아웃'):
         st.session_state.logged_in = False
         st.rerun()  # 페이지를 새로고침하여 로그인 화면으로 돌아감
-
+        
 else:
     # 로그인이 되지 않은 경우, 로그인 페이지로 리디렉션 또는 메시지 표시 
     st.error("로그인이 필요합니다.")
