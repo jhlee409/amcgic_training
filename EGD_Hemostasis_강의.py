91d7f99b78721f3c200790906010c3a77816048b
jhlee409 <jhlee409@gmail.com>
2025-02-04 09:02:49 +0900

    91

--- EGD_Hemostasis_강의.py
+++ EGD_Hemostasis_강의.py
@@ -36,7 +36,8 @@ if st.session_state.get('logged_in'):
     with st.expander(" 필독!!! 먼저 여기를 눌러 사용방법을 확인하세요."):
         st.write("- 가장 먼저 왼쪽 sidebar 선택버튼에서 default는 'Default'입니다. Hemostasis 강의, esophagus, stomach, duodenum 중 한 가지를 선택합니다.")
         st.write("- 우선 EGD 지혈술의 overview 강의를 시청하고 싶으면, Hemostasis 일반 강의를 선택하고 '진행' 버튼을 누르면 강의 화면이 나타납니다. 클릭해서 시청하세요.")
-        st.write("- EGD 지혈술 case를 시청하고 싶으면 cases 버튼을 선택하고 드롭다운 메뉴에서 case를 선택하면 치료전 case 동영상과 질문이 나타납니다.")
+        st.write("- EGD 지혈술 case를 시청하고 싶으면 우선 각 장기를 선택합니다. esophagus는 아직 증례가 올려져 있지 않아 오류납니다. 그냥 지나가세요.")
+        st.write("- 다음으로, 그 아래에서 pre_video를 선택하면 치료전 case 동영상과 질문이 나타납니다.")
         st.write("- 잘 생각해 보고 '진행' 버튼을 누르면 답과 시술 동영상을 볼수있는 동영상 player가 나타납니다.")
         st.write("- 동영상을 클릭해서 보시면 됩니다.")
         st.write("* 이 웹페이지의 출석이 기록됩니다. 끝낼 때는 반드시 좌측 하단 로그아웃 버튼을 눌러서 종결하세요.")
@@ -72,7 +73,7 @@ if st.session_state.get('logged_in'):
         return '\n'.join(full_text)
     
     # esophagus or stomach selection
-    folder_selection = st.sidebar.radio("선택 버튼", ["Default", "Hemostasis 일반 강의", "cases"])
+    folder_selection = st.sidebar.radio("선택 버튼", ["Default", "Hemostasis 일반 강의", "esophagus 증례(예정)", "stomach 증례", "duodenum 증례"])
     
     directory_videos = "EGD_Hemostasis_training/videos/"
 
@@ -86,9 +87,15 @@ if st.session_state.get('logged_in'):
     elif folder_selection == "Hemostasis 일반 강의":
         directory_pre_videos = "EGD_Hemostasis_training/lecture/video/"
         directory_instructions = "EGD_Hemostasis_training/lecture/instruction/"
-    elif folder_selection == "cases":
-        directory_pre_videos = "EGD_Hemostasis_training/cases/pre_videos/"
-        directory_instructions = "EGD_Hemostasis_training/cases/instructions/"
+    elif folder_selection == "esophagus 증례(예정)":
+        directory_pre_videos = "EGD_Hemostasis_training/esophagus/pre_videos/"
+        directory_instructions = "EGD_Hemostasis_training/esophagus/instructions/"
+    elif folder_selection == "stomach 증례":
+        directory_pre_videos = "EGD_Hemostasis_training/stomach/pre_videos/"
+        directory_instructions = "EGD_Hemostasis_training/stomach/instructions/"
+    else:
+        directory_pre_videos = "EGD_Hemostasis_training/duodenum/pre_videos/"
+        directory_instructions = "EGD_Hemostasis_training/duodenum/instructions/"
 
     st.sidebar.divider()
 
