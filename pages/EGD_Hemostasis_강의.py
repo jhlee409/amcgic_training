fc88f0295d1e010d804da5cdab6aa78b8292dffa
jhlee409 <jhlee409@gmail.com>
2025-02-18 10:15:45 +0900

    5

--- pages/EGD_Hemostasis_강의.py
+++ pages/EGD_Hemostasis_강의.py
@@ -75,19 +75,22 @@ if st.session_state.get('logged_in'):
     # esophagus or stomach selection
     folder_selection = st.sidebar.radio("선택 버튼", ["Default", "Hemostasis lecture", "cases"])
 
-            # 동영상 플레이어를 렌더링할 컨테이너 생성
+    # 동영상 플레이어를 렌더링할 컨테이너 생성
     pre_video_container = st.container()
     video_player_container = st.container()
 
     if folder_selection == "Default":
-        directory_pre_videos = "EGD_Hemostasis_training/default/pre_videos/"
+        directory_prevideos = "EGD_Hemostasis_training/default/prevideos/"
         directory_instructions = "EGD_Hemostasis_training/default/instructions/"
+        directory_videos = "EGD_Hemostasis_training/default/videos/"
     elif folder_selection == "NVUGIB overview":
-        directory_pre_videos = "EGD_Hemostasis_training/lecture/video/"
+        directory_prevideos = "EGD_Hemostasis_training/lecture/video/"
         directory_instructions = "EGD_Hemostasis_training/lecture/instruction/"
+        directory_videos = "EGD_Hemostasis_training/lecture/videos/"
     elif folder_selection == "cases":
-        directory_pre_videos = "EGD_Hemostasis_training/cases/pre_videos/"
+        directory_prevideos = "EGD_Hemostasis_training/cases/prevideos/"
         directory_instructions = "EGD_Hemostasis_training/cases/instructions/"
+        directory_videos = "EGD_Hemostasis_training/cases/videos/"
 
     # 동영상 플레이어를 렌더링할 컨테이너 생성
     if 'prevideo_container' not in st.session_state:
