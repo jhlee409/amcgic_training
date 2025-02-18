246ce6314eb910009ec3ad53637413b0b3d9e2d7
jhlee409 <jhlee409@gmail.com>
2025-02-18 12:23:25 +0900

    49

--- pages/EGD_Hemostasis_강의.py
+++ pages/EGD_Hemostasis_강의.py
@@ -131,7 +131,7 @@ if st.session_state.get('logged_in'):
                 expiration_time = datetime.now(timezone.utc) + timedelta(seconds=1600)
                 video_url = blob.generate_signed_url(expiration=expiration_time, method='GET')
                 video_html = f"""
-                    <div style="width: 700px; margin: auto;">
+                    <div style="width: 800px; margin: auto;">
                         <video style="width: 100%; height: auto;" controls src="{video_url}">
                             Your browser does not support the video element.
                         </video>
