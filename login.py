eb8cc630c2038526de54abc1e0c4c8d82920729e
jhlee409 <jhlee409@gmail.com>
2025-08-15 13:49:55 +0900

    162

--- login.py
+++ login.py
@@ -5,14 +5,14 @@ import streamlit as st
 
 # Create pages
 login_page = st.Page("pages/Login_page.py", title="로그인 페이지", icon=":material/domain:")
-page_1 = st.Page("pages/01_Dx_EGD_실전_강의", title="01_Dx_EGD_실전_강의", icon=":material/domain:")
-page_2 = st.Page("pages/02_EGD_variation_강의", title="02_EGD_variation_강의", icon=":material/domain:")
-page_3 = st.Page("pages/03_EGD_Lesion_Dx_훈련", title="03_EGD_Lesion_Dx_훈련", icon=":material/domain:")
-page_4 = st.Page("pages/04_Em_EGD_강의", title="04_Em_EGD_강의", icon=":material/domain:")
-page_5 = st.Page("pages/05_Dx_EUS_강의", title="05_Dx_EUS_강의", icon=":material/domain:")
-page_6 = st.Page("pages/06_other_lecture", title="06_other_lecture", icon=":material/domain:")
-page_7 = st.Page("pages/07_AI_patient_Hx_taking_훈련", title="07_AI_patient_Hx_taking_훈련", icon=":material/domain:")
-page_8 = st.Page("pages/08_PBL_for_GIC_F2", title="08_PBL_for_GIC_F2", icon=":material/domain:")
+page_1 = st.Page("pages/01_Dx_EGD_실전_강의.py", title="01_Dx_EGD_실전_강의", icon=":material/domain:")
+page_2 = st.Page("pages/02_EGD_variation_강의.py", title="02_EGD_variation_강의", icon=":material/domain:")
+page_3 = st.Page("pages/03_EGD_Lesion_Dx_훈련.py", title="03_EGD_Lesion_Dx_훈련", icon=":material/domain:")
+page_4 = st.Page("pages/04_Em_EGD_강의.py", title="04_Em_EGD_강의", icon=":material/domain:")
+page_5 = st.Page("pages/05_Dx_EUS_강의.py", title="05_Dx_EUS_강의", icon=":material/domain:")
+page_6 = st.Page("pages/06_other_lecture.py", title="06_other_lecture", icon=":material/domain:")
+page_7 = st.Page("pages/07_AI_patient_Hx_taking_훈련.py", title="07_AI_patient_Hx_taking_훈련", icon=":material/domain:")
+page_8 = st.Page("pages/08_PBL_for_GIC_F2.py", title="08_PBL_for_GIC_F2", icon=":material/domain:")
 
 
 # Set up navigation with sections
